ee5cdb0142278c9f2a8df397272d0d8c81a4ca49
Jad Chaar <jad.Chaar@gmail.com>
2016-08-11 13:42:57 -0400

    Added JS to fix link styling bug after click on mobile devices.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     $("a#instagram_link").click(function() {
-        $("a#instagram_link").wrap("<div class='stop_animation'></div>");
+        $("a#twitter_link").wrap("<div class='restore_styles'></div>");
+        $("a#instagram_link").wrap("<div class='restore_styles'></div>");
+        $("a#linkedin_link").wrap("<div class='restore_styles'></div>");
     });
 });
