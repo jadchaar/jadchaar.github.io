cf4d130565fc894788e64a04ebddaad6c6fb992e
Jad Chaar <jad.Chaar@gmail.com>
2016-08-11 13:49:52 -0400

    Removed JS and simplified animations

--- assets/js/scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("a#instagram_link").click(function() {
-        $("a#twitter_link").wrap("<div class='restore_styles'></div>");
-        $("a#instagram_link").wrap("<div class='restore_styles'></div>");
-        $("a#linkedin_link").wrap("<div class='restore_styles'></div>");
-    });
-});
